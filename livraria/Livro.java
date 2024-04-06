1875543fa901785bac913e5788c4d2526d9052ae
juhogino <116019433+juhogino@users.noreply.github.com>
2024-04-06 11:14:25 -0300

    Add files via upload

---
+++ livraria/Livro.java
@@ -0,0 +1,17 @@
+
+public class Livro {
+    
+    public int id;
+    public String titulo;
+    String nomeAutor;
+    public String anoPublicacao;
+    public String editora;
+
+    public Livro(int id, String titulo, String nomeAutor, String anoPublicacao, String editora){
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.anoPublicacao = anoPublicacao;
+        this.editora = editora;
+    }
+}
